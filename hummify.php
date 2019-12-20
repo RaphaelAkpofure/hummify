2f74849f0d54d36d324b33d4c29d11761953f124
RaphaelAkpofure <56492220+RaphaelAkpofure@users.noreply.github.com>
2019-12-20 02:34:49 +0100

    Update hummify.php

--- hummify.php
+++ hummify.php
@@ -2,114 +2,66 @@
 /**
  * Plugin Name: Hummify
  * Plugin URI: http://www.blizywap.com.ng/hummify
- * Description: Simple wordpress plugin to display thank you on every post.
- * Version: 1.0.0
- * Author: Raphael Akpofure
- * Author URI: http://www.mywebsite.com
- */
-/**
- * Adds a view to the post being viewed
- *
- * Finds the current views of a post and adds one to it by updating
- * the postmeta. The meta key used is "awepop_views".
- *
- * @global object $post The post object
- * @return integer $new_views The number of views the post has
+ * Descriptionnew_views The number of views the post has
  *
  */
-function awepop_add_view() {
-   if(is_single()) {
-      global $post;
-      $current_views = get_post_meta($post->ID, "awepop_views", true);
-      if(!isset($current_views) OR empty($current_views) OR !is_numeric($current_views) ) {
-         $current_views = 0;
-      }
-      $new_views = $current_views + 1;
-      update_post_meta($post->ID, "awepop_views", $new_views);
-      return $new_views;
-   }
+// Register the Custom Music Review Post Type
+ 
+function register_cpt_music_review() {
+ 
+    $labels = array(
+        'name' => _x( 'Music Reviews', 'music_review' ),
+        'singular_name' => _x( 'Music Review', 'music_review' ),
+        'add_new' => _x( 'Add New', 'music_review' ),
+        'add_new_item' => _x( 'Add New Music Review', 'music_review' ),
+        'edit_item' => _x( 'Edit Music Review', 'music_review' ),
+        'new_item' => _x( 'New Music Review', 'music_review' ),
+        'view_item' => _x( 'View Music Review', 'music_review' ),
+        'search_items' => _x( 'Search Music Reviews', 'music_review' ),
+        'not_found' => _x( 'No music reviews found', 'music_review' ),
+        'not_found_in_trash' => _x( 'No music reviews found in Trash', 'music_review' ),
+        'parent_item_colon' => _x( 'Parent Music Review:', 'music_review' ),
+        'menu_name' => _x( 'Music Reviews', 'music_review' ),
+    );
+ 
+    $args = array(
+        'labels' => $labels,
+        'hierarchical' => true,
+        'description' => 'Music reviews filterable by genre',
+        'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes' ),
+        'taxonomies' => array( 'genres' ),
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-format-audio',
+        'show_in_nav_menus' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => false,
+        'has_archive' => true,
+        'query_var' => true,
+        'can_export' => true,
+        'rewrite' => true,
+        'capability_type' => 'post'
+    );
+ 
+    register_post_type( 'music_review', $args );
 }
-add_action("wp_head", "awepop_add_view");
-
-/**
- * Retrieve the number of views for a post
- *
- * Finds the current views for a post, returning 0 if there are none
- *
- * @global object $post The post object
- * @return integer $current_views The number of views the post has
- *
- */
-function awepop_get_view_count() {
-   global $post;
-   $current_views = get_post_meta($post->ID, "awepop_views", true);
-   if(!isset($current_views) OR empty($current_views) OR !is_numeric($current_views) ) {
-      $current_views = 0;
-   }
-
-   return $current_views;
+ 
+add_action( 'init', 'register_cpt_music_review' );
+function genres_taxonomy() {
+    register_taxonomy(
+        'genres',
+        'music_review',
+        array(
+            'hierarchical' => true,
+            'label' => 'Genres',
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'genre',
+                'with_front' => false
+            )
+        )
+    );
 }
-/**
- * Shows the number of views for a post
- *
- * Finds the current views of a post and displays it together with some optional text
- *
- * @global object $post The post object
- * @uses awepop_get_view_count()
- *
- * @param string $singular The singular term for the text
- * @param string $plural The plural term for the text
- * @param string $before Text to place before the counter
- *
- * @return string $views_text The views display
- *
- */
-function awepop_show_views($singular = "view", $plural = "views", $before = "This post has: ") {
-   global $post;
-   $current_views = awepop_get_view_count();
-
-   $views_text = $before . $current_views . " ";
-
-   if ($current_views == 1) {
-      $views_text .= $singular;
-   }
-   else {
-      $views_text .= $plural;
-   }
-
-   echo $views_text;
-
-}
-/**
- * Displays a list of posts ordered by popularity
- *
- * Shows a simple list of post titles ordered by their view count
- *
- * @param integer $post_count The number of posts to show
- *
- */
- function awepop_popularity_list($post_count = 10) {
-  $args = array(
-    "posts_per_page" => 10,
-    "post_type" => "post",
-    "post_status" => "publish",
-    "meta_key" => "awepop_views",
-    "orderby" => "meta_value_num",
-    "order" => "DESC"
-  );
-
-  $awepop_list = new WP_Query($args);
-
-  if($awepop_list->have_posts()) { echo "<ul>"; }
-
-  while ( $awepop_list->have_posts() ) : $awepop_list->the_post();
-    echo '<li><a href="'.get_permalink($post->ID).'">'.the_title(’, ’, false).'</a></li>';
-  endwhile;
-
-  if($awepop_list->have_posts()) { echo "</ul>";}
- }
-
-if (function_exists("awepop_popularity_list")) {
-   awepop_popularity_list();
-}
-
+add_action( 'init', 'genres_taxonomy');
